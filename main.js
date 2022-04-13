b1e69820b875b4bec812315cce7ea4486a32cf5b
Mike Haklander <mike@the-pack.nl>
2022-04-13 18:33:33 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+function mh9gagcontrols_updateControls() {
+	var videos = document.querySelectorAll("video");
+	for (var i = 0; i < videos.length; i++) {
+		videos[i].controls = true;
+	}
+
+	var soundToggles = document.querySelectorAll(".sound-toggle");
+	for (var j = 0; j < soundToggles.length; j++) {
+		soundToggles[j].display = "none";
+		soundToggles[j].remove();
+	}
+}
+
+function mh9gagcontrols_debounce(func, timeout) {
+	var timer;
+	return function () {
+		clearTimeout(timer);
+		timer = setTimeout(function () {
+			func.apply(this);
+		}, timeout);
+	};
+}
+
+const mh9gagcontrols_onScroll = mh9gagcontrols_debounce(function () {
+	mh9gagcontrols_updateControls();
+}, 100);
+
+window.addEventListener("scroll", mh9gagcontrols_onScroll);
